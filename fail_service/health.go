22f3e74b840ddbf28e0a854320016b0d6d054cd8
Thomas Obenaus <obenaus.thomas@gmail.com>
2018-12-09 22:12:26 +0100

    .Update added timed health check

--- fail_service/health.go
+++ fail_service/health.go
@@ -1,21 +1,118 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 )
 
-type FailService struct {
-	HealthyFor   int
-	HealthyIn    int
-	UnHealthyFor int
+type FailService interface {
+	ServeHTTP(w http.ResponseWriter, r *http.Request)
+	Start()
+	Stop()
 }
 
-func (p *FailService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	w.WriteHeader(http.StatusOK)
+type failServiceImpl struct {
+	healthyFor     int64
+	healthyIn      int64
+	unHealthyFor   int64
+	ticker         *time.Ticker
+	healthy        bool
+	changeStateAt  int64
+	wasHealthyOnce bool
 }
 
-func NewFailService() FailService {
+func (fs *failServiceImpl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	return FailService{}
+	if fs.healthy {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusGatewayTimeout)
+	}
+}
+
+func (fs *failServiceImpl) Stop() {
+	fs.ticker.Stop()
+}
+
+func (fs *failServiceImpl) Start() {
+
+	currentTime := time.Now().Unix()
+	fs.changeStateAt = fs.nextEvalStateChange(currentTime)
+
+	fs.ticker = time.NewTicker(time.Millisecond * 1000)
+	go func() {
+		for _ = range fs.ticker.C {
+
+			currentTime := time.Now().Unix()
+			if fs.isChangeState(currentTime) {
+				fs.switchHealthy()
+				fs.changeStateAt = fs.nextEvalStateChange(currentTime)
+
+				log.Printf("State changed")
+				log.Printf("Next state change at %s", time.Unix(fs.changeStateAt, 0).String())
+			}
+
+			state := "healthy"
+			if !fs.healthy {
+				state = "unhealthy"
+			}
+			log.Printf("State %s", state)
+		}
+	}()
+}
+
+func (fs *failServiceImpl) switchHealthy() {
+	if !fs.wasHealthyOnce && fs.healthy {
+		fs.wasHealthyOnce = true
+	}
+	fs.healthy = !fs.healthy
+}
+
+func (fs *failServiceImpl) isChangeState(currentTime int64) bool {
+
+	if fs.healthy && fs.healthyFor == 0 {
+		return false
+	}
+
+	if currentTime > fs.changeStateAt {
+		return true
+	}
+	return false
+}
+
+func (fs *failServiceImpl) nextEvalStateChange(currentTime int64) int64 {
+
+	// currently healthy ... stay healthy for ...
+	if fs.healthy {
+		return currentTime + fs.healthyFor
+	}
+
+	// currently not healthy + were never healthy before ... initially get healthy
+	if !fs.wasHealthyOnce {
+		return currentTime + fs.healthyIn
+	}
+
+	// currently not healthy ... stay unhealthy for ...
+	return currentTime + fs.unHealthyFor
+}
+
+func NewFailService(healthyIn int64, healthyFor int64, unHealthyFor int64) FailService {
+
+	healthy := false
+	// immedately start healthy
+	if healthyIn == 0 {
+		healthy = true
+	}
+
+	result := &failServiceImpl{
+		healthyIn:      healthyIn,
+		healthyFor:     healthyFor,
+		unHealthyFor:   unHealthyFor,
+		healthy:        healthy,
+		wasHealthyOnce: healthy,
+		changeStateAt:  0,
+	}
+
+	return result
 }
--- fail_service/main.go
+++ fail_service/main.go
@@ -12,16 +12,18 @@ func main() {
 
 	var localPort = flag.Int("p", 8080, "The port where the application instance listens to. Defaults to 8080.")
 	var healtyFor = flag.Int("healthy-for", 0, "Number of seconds the health end-point will return a 200. 0 means forever.")
-	//var healtyIn = flag.Int("healthy-in", 0, "Number of seconds the health end-point will return a 200. 0 means forever.")
-	//var unhealtyFor = flag.Int("unhealthy-for", 0, "Number of seconds the health end-point will return a 200. 0 means forever.")
+	var healtyIn = flag.Int("healthy-in", 0, "Number of seconds the health end-point will start returning a 200.")
+	var unhealtyFor = flag.Int("unhealthy-for", 0, "Number of seconds the health end-point will keep returning a !200.")
 	flag.Parse()
 
 	log.Println("Cfg:")
+	log.Printf("\thealtyIn: %d", *healtyIn)
 	log.Printf("\thealtyFor: %d", *healtyFor)
+	log.Printf("\tunhealtyFor: %d", *unhealtyFor)
 
-	failService := NewFailService()
-
-	http.Handle("/health", &failService)
+	failService := NewFailService(int64(*healtyIn), int64(*healtyFor), int64(*unhealtyFor))
+	http.Handle("/health", failService)
+	failService.Start()
 
 	//start the web server
 	log.Printf("Starts listening at %d.\n", *localPort)
@@ -30,5 +32,6 @@ func main() {
 		log.Fatal("ListenAndServer:", err)
 	}
 
+	failService.Stop()
 	log.Println("Exiting")
 }
