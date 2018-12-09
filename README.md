0ec32634e8bfa9867f23f05fe6f63467f6377bc4
Thomas Obenaus <obenaus.thomas@gmail.com>
2018-12-09 16:00:38 +0100

    .Update added fail_service

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Dummy-Services
 
-[![CircleCI](https://circleci.com/gh/ThomasObenaus/dummy-services.svg?style=svg)](https://circleci.com/gh/ThomasObenaus/dummy-services)s
+[![CircleCI](https://circleci.com/gh/ThomasObenaus/dummy-services.svg?style=svg)](https://circleci.com/gh/ThomasObenaus/dummy-services)
+
+## Services
+
+- [ping_service](ping_service/)
+- [fail_service](fail_service/)
---
+++ fail_service/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/namsral/flag"
+)
+
+const version = "v1"
+
+func main() {
+
+	var localPort = flag.Int("p", 8080, "The port where the application instance listens to. Defaults to 8080.")
+	flag.Parse()
+
+	//start the web server
+	log.Printf("Starts listening at %d.\n", *localPort)
+
+	if err := http.ListenAndServe(":"+strconv.Itoa(*localPort), nil); err != nil {
+		log.Fatal("ListenAndServer:", err)
+	}
+
+	log.Println("Exiting")
+}
