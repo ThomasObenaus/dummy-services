d520859150ef55742405c7bda858318658735cf0
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-01-02 13:21:30 +0100

    .Update added retry parameter and consul kv as storage backend

--- fail_service/main.go
+++ fail_service/main.go
@@ -11,17 +11,27 @@ import (
 func main() {
 
 	var localPort = flag.Int("p", 8080, "The port where the application instance listens to. Defaults to 8080.")
-	var healtyFor = flag.Int("healthy-for", 0, "Number of seconds the health end-point will return a 200. 0 means forever.")
-	var healtyIn = flag.Int("healthy-in", 0, "Number of seconds the health end-point will start returning a 200.")
-	var unhealtyFor = flag.Int("unhealthy-for", 0, "Number of seconds the health end-point will keep returning a !200.")
+	var healthyFor = flag.Int("healthy-for", 0, "Number of seconds the health end-point will return a 200. 0 means forever.")
+	var healthyIn = flag.Int("healthy-in", 0, "Number of seconds the health end-point will start returning a 200.")
+	var unhealthyFor = flag.Int("unhealthy-for", 0, "Number of seconds the health end-point will keep returning a !200.")
+	var restartsToGetHealthy = flag.Int("restarts-to-get-healthy", 0, "The number of restarts the service waits until he starts with the procedure and gets healthy after 'healthy-in'. For this feature consul KV is needed as persistence component. The location of consul can be specified using 'consul-server-addr'.")
+	var addrOfConsul = flag.String("consul-server-addr", "", "The addr of the consul-server. Defaults to \"\". If not given the provider is searched using DNS.")
 	flag.Parse()
 
 	log.Println("Cfg:")
-	log.Printf("\thealtyIn: %d", *healtyIn)
-	log.Printf("\thealtyFor: %d", *healtyFor)
-	log.Printf("\tunhealtyFor: %d", *unhealtyFor)
+	log.Printf("\thealthyIn: %d", *healthyIn)
+	log.Printf("\thealthyFor: %d", *healthyFor)
+	log.Printf("\tunhealthyFor: %d", *unhealthyFor)
+	log.Printf("\trestartsToGetHealthy: %d", *restartsToGetHealthy)
 
-	failService := NewFailService(int64(*healtyIn), int64(*healtyFor), int64(*unhealtyFor))
+	addrOfConsulMsg := "N/A"
+	if len(*addrOfConsul) > 0 {
+		addrOfConsulMsg = *addrOfConsul
+	}
+
+	log.Printf("\taddrOfConsul: %s", addrOfConsulMsg)
+
+	failService := NewFailService(int64(*healthyIn), int64(*healthyFor), int64(*unhealthyFor))
 	http.Handle("/health", failService)
 	failService.Start()
 
